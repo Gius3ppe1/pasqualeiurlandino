f0ebf1c886beb1536dd2454e975a1e893b1185d5
Gius3ppe1 <g.moramarco20@studenti.uniba.it>
2024-02-07 12:14:40 +0100

    Fix Descriptions

--- src/pages/PersonalTrainer.jsx
+++ src/pages/PersonalTrainer.jsx
@@ -29,8 +29,8 @@ const PersonalTrainer = () => {
         <strong className="yellow">Personal </strong>Trainer
         </h1>
         <p style={{ color: "white", fontSize: "18px", paddingTop: "2%"}}>
-        Benvenuto nella mia pagina dedicata alle sedute fisiche personalizzate! <br/>Ti offro un servizio di allenamento fisico altamente personalizzato, in base alle tue esigenze e obiettivi specifici. Le sessioni si svolgono face to face, in una palestra ben attrezzata, offrendoti un'esperienza diretta e mirata. <br/>Oltre agli allenamenti, riceverai consigli costanti sulla corretta esecuzione degli esercizi, la postura e l'intensità. <br/><br/>
-        Perché Scegliere le Mie Sedute Fisiche?<br/>
+        Benvenuto nella mia pagina dedicata alle sedute personalizzate! <br/>Ti offro un servizio di allenamento fisico altamente personalizzato, in base alle tue esigenze e obiettivi specifici. Le sessioni si svolgono face to face, in una palestra ben attrezzata, offrendoti un'esperienza diretta e mirata. <br/>Oltre agli allenamenti, riceverai consigli costanti sulla corretta esecuzione degli esercizi, la postura e l'intensità. <br/><br/>
+        Perché Scegliere le Mie Sedute Personal?<br/>
         <span className="yellow">Personalizzazione Assicurata</span>, Ogni individuo è unico, e le tue esigenze di fitness sono uniche quanto te. Le mie sedute sono completamente personalizzate per adattarsi al tuo livello di forma fisica attuale, ai tuoi obiettivi specifici e alle tue preferenze.<br/>
         <span className="yellow"> Motivazione Costante</span>, mantenersi motivati è fondamentale per il successo nel fitness. Con il mio supporto costante e le sedute stimolanti, ti aiuterò a superare gli ostacoli e a mantenere alta la tua motivazione lungo tutto il percorso.<span className="yellow"> Varietà nell'Allenamento</span>, la noia è il nemico del progresso. Varieremo costantemente il tuo programma di allenamento per mantenerlo interessante e sfidante, evitando la monotonia e massimizzando i risultati.
         </p>
